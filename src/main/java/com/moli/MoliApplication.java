942ad7c7c2c1c9404079fe760a908d38784a7cd3
974751082@qq.com <974751082@qq.com>
2019-07-10 21:07:56 +0800

    init

--- src/main/java/com/moli/MoliApplication.java
+++ src/main/java/com/moli/MoliApplication.java
@@ -1,4 +1,10 @@
 package com.moli;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MoliApplication {
+    public static void main(String[] args) {
+
+    }
 }
