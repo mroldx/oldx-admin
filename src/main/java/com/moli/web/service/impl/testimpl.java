fb8cda9f44d9f2adf7ea84d44c604ba344390bb8
974751082@qq.com <974751082@qq.com>
2019-09-05 22:25:42 +0800

    test

--- src/main/java/com/moli/web/service/impl/testimpl.java
+++ src/main/java/com/moli/web/service/impl/testimpl.java
@@ -5,6 +5,7 @@ import com.moli.web.service.testservice;
 public class testimpl implements testservice {
     @Override
     public int a() {
+        System.out.println(6666);
         return 0;
     }
 }
