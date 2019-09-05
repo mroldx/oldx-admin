acff24ae030274bd85e9083c394995b3374edcbc
974751082@qq.com <974751082@qq.com>
2019-09-05 22:22:07 +0800

    test

---
+++ src/main/java/com/moli/web/service/impl/testimpl.java
@@ -0,0 +1,10 @@
+package com.moli.web.service.impl;
+
+import com.moli.web.service.testservice;
+
+public class testimpl implements testservice {
+    @Override
+    public int a() {
+        return 0;
+    }
+}
