9109989a2d25964b51d0e07f3db850047f126230
xzf520 <974751082@qq.com>
2020-04-30 16:21:32 +0800

    增加common层代码

---
+++ oldx-common/src/main/java/cc/oldx/common/core/BaseController.java
@@ -0,0 +1,10 @@
+package  cc.oldx.common.core;
+
+
+/**
+ * 通用Conteoller数据处理层
+ */
+public class BaseController
+{
+       
+}
---
+++ oldx-system/src/main/java/cc/oldx/modules/system/controller/RoleController.java
@@ -0,0 +1,18 @@
+package cc.oldx.modules.system.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author: moli
+ * @Email: 974751082@qq.com
+ * @qq: 974751082
+ * @Date: 2020/4/30 14:42
+ */
+
+
+@Controller
+@RequestMapping("/sys/role")
+ public class RoleController{
+     
+ }
