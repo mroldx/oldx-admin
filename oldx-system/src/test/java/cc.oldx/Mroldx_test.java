774efde779c23883551c4afdc0039d3adb2a0282
974751082@qq.com <974751082@qq.com>
2020-07-07 22:58:28 +0800

    增加工具类

---
+++ oldx-system/src/test/java/cc.oldx/Mroldx_test.java
@@ -0,0 +1,33 @@
+package cc.oldx;
+
+import cc.oldx.common.utils.MD5Util;
+import cc.oldx.common.utils.RedisUtil;
+import cn.hutool.core.util.RandomUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Author: moli
+ * @Email: 974751082@qq.com
+ * @qq: 974751082
+ * @Date: 2020/7/7 22:42
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class Mroldx_test {
+    @Autowired
+    private RedisUtil redisUtil;
+    @Test
+    public void testRedis(){
+        String code = RandomUtil.randomString("fdsafsd6464716f7dafds7fs4af6",4);
+        String key = MD5Util.MD5Encode(code+System.currentTimeMillis(), "utf-8");
+        redisUtil.set(key, code, 60);
+        System.out.println(1);
+
+        Object o = redisUtil.get(key);
+        System.out.println(o);
+    }
+}
